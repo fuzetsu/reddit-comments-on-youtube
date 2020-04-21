1d8ddf15114de260eb1fde06420d7986c1b141e4
Daniel Loomer <fuzetsu@users.noreply.github.com>
2020-04-20 23:33:38 -0400

    add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ['eslint:recommended'],
+  globals: {},
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [],
+  rules: {}
+}
