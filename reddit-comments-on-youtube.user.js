bd47f42a4e0d89b591dd83ab36e61ddeedc678d5
Daniel Loomer <fuzetsu@users.noreply.github.com>
2020-04-20 23:34:02 -0400

    0.2.6

--- reddit-comments-on-youtube.user.js
+++ reddit-comments-on-youtube.user.js
@@ -2,7 +2,7 @@
 // @name        Reddit Comments on Youtube
 // @description show reddit comments on youtube (and crunchyroll) videos
 // @namespace   RCOY
-// @version     0.2.5
+// @version     0.2.6
 // @match       *://*.youtube.com/*
 // @match       *://*.crunchyroll.com/*
 // @match       *://*.9anime.ru/*
