29ce3e1a3e0b0b1821e209573a94063dabc001b5
Daniel Loomer <fuzetsu@users.noreply.github.com>
2021-06-06 23:52:52 -0400

    only scrape epNum if necessary

--- src/conf/crunchyroll.ts
+++ src/conf/crunchyroll.ts
@@ -11,15 +11,16 @@ export const crunchyroll: Conf = {
   commentSelector: '.guestbook.comments',
   isMatch: () => !!getById('showmedia_about_media'),
   getPosts: async () => {
-    const epNum = q('#showmedia_about_media h4:last-child')
-      ?.textContent?.split(',')
-      .pop()
-      ?.match(/[0-9]+/)?.[0]
     const animeName = getById('showmedia_about_media')?.textContent?.replace(/\s+/g, ' ')
     if (!animeName) {
       logError('unable to find anime name')
       return []
     }
+
+    const epNum = q('#showmedia_about_media h4:last-child')
+      ?.textContent?.split(',')
+      .pop()
+      ?.match(/[0-9]+/)?.[0]
     const posts = await searchPosts(animeName + ' discussion')
     return epNum ? filterForEp(posts, epNum) : posts
   }
