db27adbd9e4eb86d490c0815372598535140d023
Daniel Loomer <fuzetsu@users.noreply.github.com>
2021-06-12 13:33:05 -0400

    add linting to dev process

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  exclude: ['./reddit-comments-on-youtube.user.js'],
   rules: {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': ['error', { ignoreRestArgs: true }],
