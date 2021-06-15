282f08dc0ba3823271b55ac4e0d4fa4b7677149c
Daniel Loomer <fuzetsu@users.noreply.github.com>
2021-06-15 00:00:04 -0400

    new (unused) util hooks

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,6 +1,20 @@
-import { useReducer } from 'preact/hooks'
+import { Inputs, useEffect, useReducer } from 'preact/hooks'
 
 export const useRedraw = () => {
   const [, redraw] = useReducer(c => c + 1, 0)
   return redraw as () => void
+}
+
+export const useTimeout = (ms: number, callback: () => void, deps: Inputs = []) => {
+  useEffect(() => {
+    const id = setTimeout(callback, ms)
+    return () => clearTimeout(id)
+  }, [ms, ...deps])
+}
+
+export const useInterval = (ms: number, callback: () => void, deps: Inputs = []) => {
+  useEffect(() => {
+    const id = setInterval(callback, ms)
+    return () => clearInterval(id)
+  }, [ms, ...deps])
 }
